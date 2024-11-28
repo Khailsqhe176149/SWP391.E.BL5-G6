cea44a4fffaf49c057e1410cd2879ecbaa37e5bf
Khailsqhe176149 <khailsqhe176149@fpt.edu.vn>
2024-11-28 13:20:51 +0700

    Update DBContext.java

--- OnlineLearning/src/java/dao/DBContext.java
+++ OnlineLearning/src/java/dao/DBContext.java
@@ -18,7 +18,7 @@ public class DBContext {
     {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
-            String url = "jdbc:sqlserver://localhost:1433;databaseName= SWP391_G6";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName= SWP391_G6;trustServerCertificate=true";
             String username = "sa";
             String password = "123";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
